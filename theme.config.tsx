8b0adb018dc62670ef112d827f773c0fd3508d19
Takuya-wool-Sato <takuya@hab-co.jp>
2024-09-18 17:37:46 +0900

    全体HAB用に変更

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>HAB Engineer documentation(仮)</span>,
   project: {
     link: 'https://github.com/shuding/nextra-docs-template',
   },
